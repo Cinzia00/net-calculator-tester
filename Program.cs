37beb6a6b4eb2df6bf28d2164363ba10bc664220
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-25 19:48:14 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace net_calculator_tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+       
+        }
+    }
+}
